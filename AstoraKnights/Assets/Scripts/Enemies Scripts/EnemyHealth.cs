3ce06b0b4bdd4ecba84b39a7fac58ffc1d311106
Saul Augusto Gasca Farrera <saulagf115@gmail.com>
2020-07-14 21:40:02 -0500

    Enemy Health
    Enemy Health and Some Skill damage scripts added

---
+++ AstoraKnights/Assets/Scripts/Enemies Scripts/EnemyHealth.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    Animator anim;
+    private float enemyHealth = 100f;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    public void TakeDamage(float damage)
+    {
+        enemyHealth -= damage;
+
+
+
+        print("Curemt enemy health: " + enemyHealth);
+
+        if(enemyHealth <= 0)
+        {
+            anim.SetBool("Death", true);
+        }
+
+    }
+}
---
+++ AstoraKnights/Assets/Scripts/FX Scripts/SkillDamage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillDamage : MonoBehaviour
+{
+
+    public LayerMask enemyMask;
+    public float radius = 0.10f;
+    public float damageCount = 10f;
+
+    EnemyHealth enemyHealth;
+    bool collided;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, radius, enemyMask);
+
+
+        foreach(Collider c in hits)
+        {
+            enemyHealth = c.gameObject.GetComponent<EnemyHealth>();
+
+
+            collided = true;
+
+
+        }
+
+
+        if(collided)
+        {
+            enemyHealth.TakeDamage(damageCount);
+            enabled = false;
+        }
+
+
+    }
+}
